6664a58fe08f721189b45aadd38fe9749ae92a4b
beingbiplov <sharmabiplov@gmail.com>
2022-07-26 09:19:36 +0545

    design login form using ant design

---
+++ forms_ant-design-setup/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import NewForm from './components/form'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NewForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
